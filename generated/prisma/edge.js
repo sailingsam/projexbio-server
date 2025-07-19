ebc769736f6b2a8ffaa8e5465e55ff7927a03436
Saksham Jain <jainsaksham1004@gmail.com>
2025-07-19 13:24:48 +0000

    trying node types depency out of devdependencies

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -296,7 +296,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -297,7 +297,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
